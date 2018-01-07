fd41c8a51cf4db595a88501e0d63a03a58dbc118
xcffl <xcffl@outlook.com>
2018-01-07 23:28:23 +0800

    Merge ClassScheduleToICS-track

--- README.md
+++ README.md
@@ -1,25 +1,16 @@
 # ClassScheduleToICS
 
-A **WebExtension** to export your class schedule as an ICS file which can be imported into external calenders.
+A Chrome extension to export your class schedule as an ICS file which can be imported into external calenders.
 
 Theoretically, it should work on any system powered by the same software (Oracle Peoplesoft, I suspect), but I currently limit the functionality to CUHKSZ (and CUHK) in the manifest file so the script doesn't run on all websites.
 
 And now, CUHK is also supported.
 
-## Install
-
-### Firefox
-
-https://addons.mozilla.org/zh-CN/firefox/addon/cuhksz-cuhk-schedule-exporter/
-
-### Chrome
-
-Refer to https://chrome.google.com/webstore/detail/cuhksz-cuhk-class-schedul/fjfegpobkpkdmcogjibdckigcnocedif
+The only difference between ClassScheduleToICS and ClassScheduleToICS-track is that ClassScheduleToICS-track contains track code which allows Qianjian to count the number of people benefited from this project.
 
 ## Download
-
-Now available on [浅见](http://qianjian.tk/2015/09/class-schedule-to-ics/).
+Now available on [浅见](https://qianjian.space/2015/09/class-schedule-to-ics/).
 
 ## Credit
 
-Originally developed by keanulee, adapted and enhanced by Alan Chen, ported to Firefox by xcffl.
+Originally developed by keanulee, adapted and enhanced by Alan Chen.
---
+++ cuhk.js
@@ -0,0 +1,54 @@
+function composeical(data) {
+    var timezone = 'Asia/Hong_Kong';
+    console.debug('Composing iCalContent');
+    // For the difficulty of determining the real start date of each day, we use a unified start
+    // date, yet set Monday into the exceptions of the classes that take in other days to cover most cases
+    if (data['daysOfWeek'] != "MO") {
+      var exdate = 'EXDATE;TZID=' + timezone + ':' + getDateTimeString(data['startDate'], data['startTime']) + '\n'
+    } else {
+      var exdate = ''
+    }
+    var iCalContent =
+        'BEGIN:VEVENT\n' +
+        'DTSTART;TZID=' + timezone + ':' + getDateTimeString(data['startDate'], data['startTime']) + '\n' +
+        //'DTSTART;TZID=' + timezone + ':' + startDateString + 'T' + getTimeString(startTime) + '\n' +
+        'DTEND;TZID=' + timezone + ':' + getDateTimeString(data['startDate'], data['endTime']) + '\n' +
+        //'DTEND;TZID=' + timezone + ':' + startDateString + 'T' + getTimeString(endTime) + '\n' +
+        'LOCATION:' + data['room'] + '\n' +
+        'RRULE:FREQ=WEEKLY;UNTIL=' + getDateTimeString(data['endDate'], data['endTime']) + 'Z;BYDAY=' + data['daysOfWeek'] + '\n' +
+        //'RRULE:FREQ=WEEKLY;UNTIL=' + endDateString + 'T' + getTimeString(endTime) + 'Z;BYDAY=' + daysOfWeek + '\n' +
+        exdate +
+        //'EXDATE;TZID=' + timezone + ':' + startDateString + 'T' + getTimeString(startTime) + '\n' +
+        'SUMMARY:' + data['courseCode'] + '(' + data['component'] + ')\n' +
+        'DESCRIPTION:' +
+        'Course Name: ' + data['courseName'] + '\\n' +
+        'Section: ' + data['section'] + '\\n' +
+        'Instructor: ' + data['instructor'] + '\\n' +
+        'Component: ' + data['component'] + '\\n' +
+        'Class Number: ' + data['classNumber'] + '\\n' +
+        'Days/Times: ' + data['daysTimes'] + '\\n' +
+        'Start/End Date: ' + data['startEndDate'] + '\\n' +
+        'Location: ' + data['room'] + '\\n\\n\\n---\\n' +
+        'Note: ' + 'Proudly brought to you by Alan Chen. If you find any mistake, please report it immediately to admin@zenan.ch or on Github as such mistake will annoy other students.' + '\\n\n' +
+        'END:VEVENT\n';
+    //console.debug(iCalContent);
+    // Remove double spaces from content.
+    iCalContent = iCalContent.replace(/\s{2,}/g, ' ');
+    return iCalContent;
+}
+
+function datebuilder(dateString) {
+    var dateArray = dateString.split('/');
+    return new Date(dateArray[2], dateArray[1] - 1, dateArray[0]);
+}
+
+var selectors = {
+    "componentRows": 'table[id^="CLASS_MTG_VW"]',
+    "classNumber": 'td:nth-child(1)>span',
+    "daysTimes": 'td:nth-child(4)>span',
+    "section": 'a[id*="MTG_SECTION"]',
+    "component": 'td:nth-child(3)>span',
+    "room": 'td:nth-child(5)>span',
+    "instructor": 'td:nth-child(6)>span',
+    "startEndDate": 'td:nth-child(7)>span'
+}
